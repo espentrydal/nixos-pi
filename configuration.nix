048a8b1c9edd218612d2fc11d7a529c27e60678d
Espen Trydal <espen@trydal.io>
2022-08-06 19:07:37 +0200

    avahi

--- configuration.nix
+++ configuration.nix
@@ -17,7 +17,7 @@ in
   # Enables the generation of /boot/extlinux/extlinux.conf
   boot.loader.generic-extlinux-compatible.enable = true;
   # !!! Set to specific linux kernel version
-  boot.kernelPackages = pkgs.linuxPackages_5_4;
+  boot.kernelPackages = pkgs.linuxPackages_5_15;
   # !!! Needed for the virtual console to work on the RPi 3, as the default of 16M doesn't seem to be enough.
   # If X.org behaves weirdly (I only saw the cursor) then try increasing this to 256M.
   # On a Raspberry Pi 4 with 4 GB, you should either disable this parameter or increase to at least 64M if you want the USB ports to work.
@@ -70,6 +70,12 @@ in
     };
   };
 
+  # Avahi
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+  };
+  # OpenSSH
   services.openssh = {
     enable = true;
     passwordAuthentication = false;
@@ -77,7 +83,7 @@ in
     openFirewall = false;
   };
 
-  # enable the tailscale daemon; this will do a variety of tasks:
+    # enable the tailscale daemon; this will do a variety of tasks:
   # 1. create the TUN network device
   # 2. setup some IP routes to route through the TUN
   services.tailscale = { enable = true; };
